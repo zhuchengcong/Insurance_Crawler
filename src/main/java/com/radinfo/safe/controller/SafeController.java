f3d36600c3eb4580cdd80b62280d2ce9ff2cd231
zhuchengcong <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>
2021-09-07 09:50:41 +0800

    update rad

--- src/main/java/com/radinfo/safe/controller/SafeController.java
+++ src/main/java/com/radinfo/safe/controller/SafeController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class SafeController {
 
-
+//test rad update
     @Autowired
     private Safeservice safeservice;
 

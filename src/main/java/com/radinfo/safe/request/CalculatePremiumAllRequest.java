56fca9be1da93c5fe836c51624e355bfdb78c4af
zhuchengcong <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>
2021-09-14 14:12:52 +0800

    update 2021 0907 17:02

---
+++ src/main/java/com/radinfo/safe/request/CalculatePremiumAllRequest.java
@@ -0,0 +1,35 @@
+package com.radinfo.safe.request;
+
+import java.io.Serializable;
+//合并计算请求体
+public class CalculatePremiumAllRequest implements Serializable {
+    private static final long serialVersionUID = -3757215755707468335L;
+//    InsuranceBIData
+    private boolean hideLoading=false;
+    private InsuranceBIDataRequest insuranceBIData;
+    private InsuranceCIDataRequest insuranceCIData;
+
+    public InsuranceCIDataRequest getInsuranceCIData() {
+        return insuranceCIData;
+    }
+
+    public void setInsuranceCIData(InsuranceCIDataRequest insuranceCIData) {
+        this.insuranceCIData = insuranceCIData;
+    }
+
+    public boolean isHideLoading() {
+        return hideLoading;
+    }
+
+    public void setHideLoading(boolean hideLoading) {
+        this.hideLoading = hideLoading;
+    }
+
+    public InsuranceBIDataRequest getInsuranceBIData() {
+        return insuranceBIData;
+    }
+
+    public void setInsuranceBIData(InsuranceBIDataRequest insuranceBIData) {
+        this.insuranceBIData = insuranceBIData;
+    }
+}
